50a36f359b502082e87bf9bbc59db07648e043c6
Michal Katuščák <michal@katuscak.cz>
2025-04-24 16:52:44 +0200

    Remove test files

--- public/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Simple test file to check if PHP is working correctly
-echo "PHP is working correctly!";
-phpinfo();
-?>
--- test.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Jednoduchý testovací soubor
-echo "Test přístupu k adresáři funguje správně!";
-?>
